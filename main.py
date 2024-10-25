aa9cda1ddbd8eb015fa3cdfcc4e1c4e0e0c7b9e9
LeylineExplorer <vinicius.parizi@hotmail.com>
2024-10-25 17:24:19 -0300

    Player movement with optional WASD or Arrow keys, update and draw in groups

--- main.py
+++ main.py
@@ -12,13 +12,25 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        for obj in updatable:
+            obj.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+            
         pygame.display.flip()
+
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
